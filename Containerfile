f34dbf9ad482dd3f4179272c0612cc40d249b343
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-10-20 14:23:53 +0200

    Initial commit

---
+++ Containerfile
@@ -0,0 +1,28 @@
+FROM docker.io/node:20.8.1 as base
+WORKDIR /app
+RUN npm config set update-notifier false
+
+FROM base as dev
+CMD exec /bin/bash -c "npm install && npm run dev"
+
+FROM base as build
+COPY package.json .
+COPY package-lock.json .
+RUN npm ci
+COPY public public
+COPY src src
+COPY env.d.ts .
+COPY index.html .
+COPY tsconfig.app.json .
+COPY tsconfig.json .
+COPY tsconfig.node.json .
+COPY tsconfig.vitest.json .
+COPY vite.config.ts .
+COPY vitest.config.ts .
+RUN npm run build
+
+FROM docker.io/nginx:stable-alpine-slim as prod
+COPY --from=build /app/dist /usr/share/nginx/html
+
+FROM scratch as dist
+COPY --from=build /app/dist /
---
+++ dev.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env sh
+
+set -e
+
+dev_image=${DEV_IMAGE:-"ldap-manager-dev"}
+container_name=ldap-manager-dev
+
+build_image() {
+    podman build \
+        --force-rm \
+        --layers \
+        --target dev \
+        --tag "${dev_image}" \
+        .
+}
+
+if [ "$1" = "build" ]; then
+    build_image
+    exit 0
+fi
+
+if ! podman image exists "$dev_image"; then
+    build_image
+fi
+
+# params given will be appended at the end of the command
+commands_given="$*"
+shift "$#"
+
+if podman container exists $container_name; then
+  set -- "$@" podman exec --interactive --tty $container_name
+else
+  set -- "$@" podman run \
+    --name $container_name \
+    --replace \
+    --rm \
+    --interactive \
+    --tty \
+    --publish 5173:5173 \
+    --publish 4173:4173 \
+    --volume "$(pwd)":/app:Z \
+    "${dev_image}"
+fi
+
+# if commands_given are not zero, append them
+if [ -n "$commands_given" ]; then
+  for command in $commands_given; do
+    set -- "$@" "$command"
+  done
+fi
+
+"$@"
