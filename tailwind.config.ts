f5841063d8b9954bf569993743317d8c4c3142d8
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-10-23 12:48:02 +0200

    build: added vue-tailwind-lib

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/@nethserver/vue-tailwind-lib/dist/vue-tailwind-lib.es.js'
+  ],
   theme: {
     extend: {}
   },
