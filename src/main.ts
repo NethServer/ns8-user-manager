7a21e1f1ce347541c40f835880d30b50cbb60276
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-10-23 17:25:30 +0200

    chore: added vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,24 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { createI18n } from 'vue-i18n'
+import enJson from './i18n/en-US.json'
+import itJson from './i18n/it-IT.json'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(
+  createI18n({
+    locale: navigator.language,
+    fallbackLocale: 'en',
+    messages: {
+      en: enJson,
+      it: itJson
+    }
+  })
+)
 
 app.mount('#app')
