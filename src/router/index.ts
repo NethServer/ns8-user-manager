b57ec69031bd7e8ababab605298a4c1b438a7f1e
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-10-25 17:57:00 +0200

    feat: added base template for application

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: () => import('../views/BaseTemplate.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: () => import('../views/HomeView.vue')
+        }
+      ]
     },
     {
       path: '/login',
---
+++ src/views/BaseTemplate.vue
@@ -0,0 +1,68 @@
+<script lang="ts" setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons/faCircleUser'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons/faAngleDown'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+
+import { useAuth } from '@/composables/useAuth'
+import router from '@/router'
+
+const { logout } = useAuth()
+
+function handleLogout() {
+  logout()
+  router.replace({ name: 'login' })
+}
+</script>
+
+<template>
+  <div class="min-w-screen flex min-h-screen bg-white dark:bg-gray-950">
+    <div
+      class="hidden w-80 flex-col gap-y-8 border-r border-gray-300 px-2 py-8 dark:border-gray-800 sm:flex"
+    >
+      <div>
+        <div class="flex flex-col items-center justify-center gap-y-1">
+          <FontAwesomeIcon
+            :icon="faCircleUser"
+            class="h-24 w-24 text-gray-700 dark:text-gray-200"
+          />
+          <div class="text-center">
+            <p class="font-medium">Placeholder</p>
+            <p class="text-sm text-gray-600">Placeholder</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex grow flex-col">
+      <div
+        class="flex h-16 min-w-full items-center gap-x-2 border-b border-gray-300 px-4 dark:border-gray-800"
+      >
+        <div class="ml-auto flex items-center text-gray-700 dark:text-gray-200">
+          <div class="mr-2 h-8 w-8">
+            <FontAwesomeIcon :icon="faCircleUser" class="h-full w-full" />
+          </div>
+          <Menu as="div" class="relative">
+            <MenuButton>
+              <FontAwesomeIcon :icon="faAngleDown" />
+            </MenuButton>
+            <MenuItems
+              class="absolute right-0 top-10 w-32 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-gray-200 dark:bg-gray-900 dark:ring-gray-700"
+            >
+              <MenuItem>
+                <button
+                  class="flex w-full px-6 py-2 text-gray-900 hover:bg-gray-100 dark:text-gray-50 dark:hover:bg-gray-700"
+                  @click="handleLogout()"
+                >
+                  {{ $t('base_template.logout') }}
+                </button>
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+        </div>
+      </div>
+      <main class="grow bg-gray-50 p-6 dark:bg-gray-900">
+        <RouterView />
+      </main>
+    </div>
+  </div>
+</template>
