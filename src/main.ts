73763f1ca0318ef778488c00a8e96bcbfeaff2c7
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-10-30 14:46:52 +0100

    fix: fixed issues with nested routing

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ app.use(
 )
 
 // setup axios
-axios.defaults.baseURL = import.meta.env.VITE_ENDPOINT ?? ''
+axios.defaults.baseURL = import.meta.env.VITE_ENDPOINT ?? './'
 axios.defaults.timeout = 500
 
 axios.interceptors.request.use((config) => {
