41c091afffd6a5327af6f7c43f8a8c490d489b2a
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2024-01-03 14:57:49 +0100

    fix: fixed issue with menu hiding when navigating

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -9,17 +9,19 @@ defineEmits(['navigate'])
 </script>
 
 <template>
-  <div class="flex flex-col space-y-2" @navigate="$emit('navigate')">
+  <div class="flex flex-col space-y-2">
     <SidebarLink
       :icon="faGear"
       :label="$t('account_settings.title')"
       :to="{ name: 'user_account' }"
+      @navigate="$emit('navigate')"
     />
     <SidebarLink
       v-if="scopes.length == 0"
       :icon="faUserFriends"
       :label="$t('user_manager.title')"
       :to="{ name: 'user_manager' }"
+      @navigate="$emit('navigate')"
     />
   </div>
 </template>
