c40333e21b3b916deb994137aee7241bac33b695
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2024-01-12 17:31:13 +0100

    build: removing dist before exporting dist

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ podman build --tag "$IMAGE" --target dist --force-rm --layers .
 container_id=$(podman create "$IMAGE" /)
 trap 'podman rm -f $container_id' EXIT
 trap 'podman image rm -f $IMAGE' EXIT
+rm -fr dist
 mkdir -p dist
 podman export "$container_id" | tar --extract --overwrite --directory dist
