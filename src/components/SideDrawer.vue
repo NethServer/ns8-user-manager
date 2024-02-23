23194befd3c0dffcd3ece9b70319de7ae7e919cb
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2024-02-23 10:39:22 +0100

    refactor: using headlessui to render dialog

--- src/components/SideDrawer.vue
+++ src/components/SideDrawer.vue
@@ -2,6 +2,7 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faX } from '@fortawesome/free-solid-svg-icons'
 import { NeButton } from '@nethserver/vue-tailwind-lib'
+import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue'
 
 defineProps<{
   show: boolean
@@ -12,51 +13,42 @@ defineEmits(['cancel'])
 </script>
 
 <template>
-  <Teleport to="body">
-    <Transition name="fade">
-      <div
-        v-if="show"
-        class="fixed bottom-0 left-0 right-0 top-0 bg-gray-600 bg-opacity-40"
-        @click="$emit('cancel')"
-      ></div>
-    </Transition>
-    <Transition name="slide">
-      <div
-        v-if="show"
-        class="fixed right-0 top-0 flex h-full w-screen flex-col gap-y-6 overflow-y-auto bg-gray-100 p-6 sm:w-[30rem] dark:bg-gray-900"
+  <TransitionRoot :show="show" as="template">
+    <Dialog @close="$emit('cancel')">
+      <TransitionChild
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
       >
-        <div class="flex gap-x-2 break-all align-baseline">
-          <p class="text-xl font-medium">{{ title }}</p>
-          <NeButton kind="tertiary" class="ml-auto self-center" @click="$emit('cancel')">
-            <FontAwesomeIcon class="text-white" :icon="faX" />
-          </NeButton>
-        </div>
-        <hr />
-        <slot></slot>
-        <hr v-if="$slots.footer" />
-        <slot name="footer"></slot>
-      </div>
-    </Transition>
-  </Teleport>
+        <div class="fixed inset-0 bg-gray-600 bg-opacity-40" />
+      </TransitionChild>
+      <TransitionChild
+        as="template"
+        enter="transition ease-in-out duration-300 transform"
+        enter-from="translate-x-full"
+        enter-to="translate-x-0"
+        leave="transition ease-in-out duration-300 transform"
+        leave-from="translate-x-0"
+        leave-to="translate-x-full"
+      >
+        <DialogPanel class="fixed inset-y-0 right-0 bg-gray-100 p-6 sm:w-[30rem] dark:bg-gray-900">
+          <div class="flex flex-col gap-y-6 overflow-y-auto">
+            <div class="flex gap-x-2 break-all align-baseline">
+              <p class="text-xl font-medium">{{ title }}</p>
+              <NeButton class="ml-auto self-center" kind="tertiary" @click="$emit('cancel')">
+                <FontAwesomeIcon :icon="faX" class="text-white" />
+              </NeButton>
+            </div>
+            <hr />
+            <slot></slot>
+            <hr v-if="$slots.footer" />
+            <slot name="footer"></slot>
+          </div>
+        </DialogPanel>
+      </TransitionChild>
+    </Dialog>
+  </TransitionRoot>
 </template>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-
-.slide-enter-active,
-.slide-leave-active {
-  transition: transform 0.3s ease;
-}
-.slide-enter-from,
-.slide-leave-to {
-  transform: translateX(100%);
-}
-</style>
