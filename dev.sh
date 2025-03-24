053229c76c4a75c9915b208eda828cf13103b123
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-03-24 17:22:48 +0100

    build: setting host as network for container

--- dev.sh
+++ dev.sh
@@ -25,7 +25,7 @@ if [ "$1" = "preview" ]; then
         --rm \
         --interactive \
         --tty \
-        --publish 4173:4173 \
+        --network host \
         "$dev_image:build" \
         npm run preview
     exit 0
@@ -54,16 +54,10 @@ if podman container exists $container_name; then
   set -- "$@" $container_name
 else
   # base command to create container
-  set -- "$@" run --name $container_name --replace --rm --volume "$(pwd)":/app:Z
+  set -- "$@" run --name $container_name --replace --rm --volume "$(pwd)":/app:Z --network host
   # if terminal is interactive, add interactive and tty flags
   if [ -t 0 ]; then
     set -- "$@" --interactive --tty
-  fi
-  # check if port 5173 is already in use, if not, add publish flag
-  if lsof -Pi :5173 -sTCP:LISTEN -t >/dev/null; then
-      echo "Something is listening on port 5173, you won't be able to reach dev build."
-  else
-      set -- "$@" --publish 5173:5173
   fi
   # add image name
   set -- "$@" "$dev_image"
