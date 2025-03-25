5aac74aea19c23739a9781f1542c2fb80081b2ce
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-03-25 14:10:49 +0100

    style: using right configuration for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,17 @@
-import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-export default [
-  js.configs.recommended,
+export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}']
   },
-
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
   },
-
-  ...pluginVue.configs['flat/recommended'],
-  ...vueTsEslintConfig(),
-
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommended,
   skipFormatting
-]
+)
